99de63e8d99463480d5f7681d3c5c99faffcb10d
박민호 <minho.park@navercorp.com>
2018-08-25 19:59:19 +0900

    스크롤뷰 추가

--- autolayoutTest/ViewController.swift
+++ autolayoutTest/ViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    weak var imgView: UIImageView!
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+    var imgView: UIImageView!
     
     var imgSrc: UIImage?
     
@@ -26,7 +27,6 @@ class ViewController: UIViewController {
         super.loadView()
         
         let imgView = UIImageView()
-        self.view.addSubview(imgView)
         //제약조건을 프로그래밍으로 할때는 뷰 자체적으로 수행하는 오토리사이징을 꺼야 함(이유 : 사용자가 지정한 오토레이아웃 조건과 충돌하여 제약조건 문제를 일으킴)
         imgView.translatesAutoresizingMaskIntoConstraints = false
    /*
@@ -44,13 +44,6 @@ class ViewController: UIViewController {
         self.view.addConstraints(horizontal)
      */
         
-        NSLayoutConstraint.activate([
-            imgView.widthAnchor.constraint(equalToConstant: self.view.bounds.width),
-            imgView.heightAnchor.constraint(equalToConstant: self.view.bounds.height),
-    //        imgView.topAnchor.constraint(equalTo: self.view.topAnchor),
-            imgView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            imgView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-            ])
         self.imgView = imgView
         
     }
@@ -59,12 +52,41 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.scrollView.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+//        self.scrollView.alwaysBounceVertical = true       //If you want, remove this comment
+        
+        self.view.addSubview(self.scrollView)
+        self.scrollView.addSubview(self.contentView)
+        self.contentView.addSubview(self.imgView)
+        
         self.imgView.backgroundColor = .yellow
         
         imgSrc = UIImage(named: "siba.jpeg")
         imgView.contentMode = .scaleAspectFit
         self.imgView.image = imgSrc
         
+        self.scrollView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        self.scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        self.scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.contentView.topAnchor.constraint(equalTo: self.scrollView.topAnchor).isActive = true
+        self.contentView.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor).isActive = true
+        self.contentView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor).isActive = true
+        self.contentView.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor).isActive = true
+        self.contentView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        
+        NSLayoutConstraint.activate([
+//            imgView.widthAnchor.constraint(equalToConstant: self.view.bounds.width),
+//            imgView.heightAnchor.constraint(equalToConstant: self.view.bounds.height),
+//                    imgView.topAnchor.constraint(equalTo: self.view.topAnchor),
+//            imgView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor),
+//            imgView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+            imgView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            imgView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            imgView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            imgView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor)
+            ])
     }
     
     /*
